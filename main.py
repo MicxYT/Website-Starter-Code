1d1ed13825f13707d3bccccc32d9ffaa14476a1f
Micx <87043050+MicxYT@users.noreply.github.com>
2021-07-11 01:27:33 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask # import flask
+
+app = Flask(__name__) # define app as flask
+
+@app.route("/") # our route eg: example.com/
+def home():
+    return "This Is The Homepage" # return text
+
+@app.rout("/help") # our route eg: example.com/help
+def help():
+    return "This is the help page" # return text
+
+app.run(host="0.0.0.0",port="80") # Run the website
